c7e87e9f372c01044966e0c2a59baa8a851b4646
Jami Griffin <jamigriffin@outlook.com>
2023-04-04 07:33:39 -0500

    Week 2 Workshop - Review Modal

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../shared/baseUrl';
 
+
 export const fetchComments = createAsyncThunk(
     'comments/fetchComments',
     async () => {
@@ -18,7 +19,11 @@ export const fetchComments = createAsyncThunk(
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload)
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -35,7 +40,21 @@ const commentsSlice = createSlice({
                     ? action.error.message
                     : 'Fetch failed';
             });
+
     }
 });
 
+export const postComment = createAsyncThunk(
+    "comments/postComment",
+    async (payload, { dispatch, getState }) => {
+      setTimeout(() => {
+        const { comments } = getState();
+        payload.date = new Date().toISOString();
+        payload.id = comments.commentsArray.length;
+        dispatch(addComment(payload));
+      }, 2000);
+    }
+  );
+
+export const { addComment } = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
